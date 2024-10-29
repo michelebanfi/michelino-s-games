7846df8c45fe6e1d2425fca0f29428f79fa89b29
michelebanfi <michi.banfi01@gmail.com>
2024-10-29 10:08:20 +0100

    Initial Commit

---
+++ Michelino's games/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Michelino's games
+//
+//  Created by Michele Banfi on 29/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Michelino's games/Michelino_s_gamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Michelino_s_gamesApp.swift
+//  Michelino's games
+//
+//  Created by Michele Banfi on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Michelino_s_gamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
